f93afa9308fd8f59d44cfde9704ea4538dad63cf
Mohammad Aman <official.aman60@gmail.com>
2024-09-05 21:45:38 +0530

    testing 1 for the repo

---
+++ src/main/java/com/aman/sample/repositorytesting1/RepositoryTesting1Application.java
@@ -0,0 +1,13 @@
+package com.aman.sample.repositorytesting1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryTesting1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryTesting1Application.class, args);
+    }
+
+}
